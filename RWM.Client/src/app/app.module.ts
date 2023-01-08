7a06356c04bfa75c2680365875bffd162ff330df
KatiPav <katerinapavlova28@gmail.com>
2023-01-08 17:01:46 +0200

    added book-page component and chenged css

--- RWM.Client/src/app/app.module.ts
+++ RWM.Client/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BookComponent } from './components/book/book.component';
+import { BookPageComponent } from './components/book-page/book-page.component';
 
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    BookPageComponent
   ],
   imports: [
     BrowserModule,
