1393a110cfed12ac7f2d42a03c25e012aa79fab1
KatiPav <katerinapavlova28@gmail.com>
2023-01-14 18:36:39 +0200

    added working book-page component and book search

--- RWM.Client/src/app/components/book-page/book-page.component.ts
+++ RWM.Client/src/app/components/book-page/book-page.component.ts
@@ -1,64 +1,46 @@
 import { Component } from '@angular/core';
 import { Comment } from '../../models/comment.model';
+import { Book } from '../../models/book.model';
 import { ActivatedRoute } from '@angular/router';
 
 import { CommentService } from '../../services/comment.service'
+import { BookService } from 'src/app/services/book.service';
 @Component({
   selector: 'app-book-page',
   templateUrl: './book-page.component.html',
   styleUrls: ['./book-page.component.css']
 })
 export class BookPageComponent {
-  private bookId: string = "d804ab37-c31d-41be-388c-08daf63cbbbb";
+  protected book: Book = {id: "",  title: "" , series: "" , author: "", description: "",  pages: 0, coverImgUrl: ""};
   protected comments: Comment[] = [];
-  constructor(private route: ActivatedRoute, private commentsService: CommentService) { }
+  constructor(private route: ActivatedRoute, private commentsService: CommentService, private bookService: BookService) { }
 
+  ngOnInit(){
+    this.route.queryParams.subscribe(params => {
+      this.bookService.getBook(params['id']).subscribe(book => {this.book = book});
+    });
+   
+  }
+  
 
   showComments() {
 
     const page = document.getElementById("book-page") as HTMLInputElement;
     page?.addEventListener('page', this.getValue);
 
-    this.commentsService.getCommentsBefore(this.bookId, +page.value)
+    this.commentsService.getCommentsBefore(this.book.id, +page.value)
       .subscribe(comments => {
-        this.comments = comments
+        this.comments = comments.reverse()
       });
-
-    this.updateComments();
   }
-
-  updateComments() {
-    const displayComments = document.getElementById('display-comments');
-
-    this.comments.forEach(comment => {
-      //creates html section elements with title and text
-      const section = document.createElement('section');
-      section.setAttribute('class', 'comment');
-
-      const info = document.createElement('p');
-      info.textContent = `${comment.username} on page ${comment.page}`;
-      info.setAttribute('class', 'username-on-comment');
-      section.appendChild(info);
-
-      const p = document.createElement('p');
-      p.textContent = comment.text;
-      section.appendChild(p);
-      displayComments?.appendChild(section);
-    })
-  };
-
 
   getValue(event: any) {
     const target = event.target as HTMLInputElement;
     console.log(target.value);
-
   }
-
 
   generateId() {
     return "3fa85f64-5717-4562-b3fc-2c963f66afa6";
-
-
   }
 
   publish() {
@@ -106,13 +88,9 @@ export class BookPageComponent {
       document.getElementById('book-page-number-warning')?.setAttribute('hidden', '');
     }
 
-    const comment: Comment = { id: this.generateId(), username: username!.value, text: commentText!.value, page: +page!.value, bookId: this.bookId };
-
-    this.comments.push(comment);
+    const comment: Comment = { id: this.generateId(), username: username!.value, text: commentText!.value, page: +page!.value, bookId: this.book.id };
+    this.comments.unshift(comment);
     this.commentsService.addComment(comment);
-    console.log("here");
-    this.updateComments();
-
 
   }
 }
